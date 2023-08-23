9dda67816bbf22b6c3c7f119862545c63fd7cbc3
VictoR-11011994 <victor.carablut@gmail.com>
2023-08-23 11:44:19 +0200

    updated

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Secure Data (Local Storage)
 import secureLocalStorage from "react-secure-storage";
